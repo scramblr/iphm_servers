33736ac97784b75dd02dd39cce15f914d86830fe
scramblr <52064980+scramblr@users.noreply.github.com>
2024-06-07 08:00:27 -0700

    Update and rename spoof_check.sh to spoof_check_apt.sh

--- spoof_check_apt.sh
+++ spoof_check_apt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#
-# released 2024
-# v0.1
+# SPOOF_CHECK
+# released June 7 2024
+# v0.1a
 #
 
-echo
+echo ''
+echo 'spoof_check_apt.sh for APTITUDE based *NIX Servers like Ubuntu, Debian, Etc. Run spoof_check_yum.sh for YUM based distros.'
 echo 'This tool will download the prerequisits and then ask for the server you have tcpdump running on before launching a small test that will prove whether the server is in fact spoofing packets.'
 echo 'spoof_check.py does NOT USE CAIDA Spoof Tools or any other tools like it, because we are not dumb. Anyone who uses tools like that are BEGGING for their providers to be shut down.'
 echo ''
@@ -15,7 +16,7 @@ apt-get install net-tools bind9-dnsutils inetutils-tools tcpdump python3-scapy -
 echo 'Tool installation complete.'
 echo ''
 echo ''
-echo 'Next, Launch the included spoof_test.py'
+echo 'Next, Launch the included spooftest.py that comes with the repo github.com/scramblr/spooftest.git'
 echo 'shell$ python3 spoof_test.py 1.2.3.4'
 echo
 echo
